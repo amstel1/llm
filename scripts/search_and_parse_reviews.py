4286911444ba3984fb4483874cafeb58d0939516
amstel <spareemail@yandex.ru>
2024-04-10 11:19:51 +0300

    begin testing multithreading

--- scripts/search_and_parse_reviews.py
+++ scripts/search_and_parse_reviews.py
@@ -1,18 +1,19 @@
 import asyncio
+import concurrent.futures
 import pickle
 import re
 import bs4
 from playwright.sync_api import sync_playwright
 from playwright.async_api import async_playwright
 from loguru import logger
-from typing import Dict, List, Callable
+from typing import Dict, List, Callable, Any
 from bs4 import BeautifulSoup
-from typing import Dict
 import extruct
 from db import select_from_db
 import rapidfuzz
 ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-
+import concurrent.futures as pool
+Url = str
 
 @logger.catch
 def parse_product(mdata) -> Dict[str, str]:
@@ -54,8 +55,8 @@ def parse_product(mdata) -> Dict[str, str]:
                 }
 
 @logger.catch
-def parse_reviews(mdata) -> str:
-    '''returns reviews delimited by /n from microdata'''
+def parse_reviews(mdata) ->  List[dict[str, Any]]:
+    '''returns reviews'''
     for el in mdata:
         if el.get('type') == 'https://schema.org/Product':
             if 'properties' in el:
@@ -74,22 +75,24 @@ def parse_reviews(mdata) -> str:
                 return total_reviews
 
 @logger.catch
-async def scrape_page_playwright(url_path: str, parse_func: Callable) -> List[Dict]:
+def scrape_page_playwright(url_path: Url, parse_func: Callable) -> List[tuple[Url, Dict | List[Dict]]]:
     parsing_output = []
-    async with async_playwright() as p:
-        browser = await p.firefox.launch(headless=True)
-        page = await browser.new_page()
-        await page.goto(url_path)
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url_path)
         # page.screenshot(path="now.png")
-        content = await page.content()
+        content = page.content()
         # with open('product.html', 'w') as f:
         #     f.write(content)
         soup = BeautifulSoup(content, "html.parser")
         mdata = extruct.extract(soup.prettify(), syntaxes=['microdata']).get('microdata')
+        # if parse_func==parse_product: parsing_output -> Dict
+        # if parse_func==parse_review: parsing_output -> List[Dict]
         parsing_output = parse_func(mdata)
         # assert parsing_output  # if error, parsing failed, check microdata of the page
-        await browser.close()
-    return parsing_output
+        browser.close()
+    return (url_path, parsing_output)
 
 
 def clear_str(s):
@@ -101,33 +104,55 @@ def google_find_link(soup, user_query) -> str:
     articles = soup.find_all(class_='MjjYud')
 
     id_2_href = {}
-    id_2_text = {}
+    id_2_text_primary = {}  # results containing 'reviews' - reliable
+    id_2_text_secondary = {}  # results not containing 'reviews' - unreliable
+
     id_2_score = {}  # simple relevancy score
-    logger.debug(f'len of articles: {len(articles)}')
+    # logger.debug(f'len of articles: {len(articles)}')
     for article_id, article in enumerate(articles):
         try:
             article_href = article.find('span').find('a').get('href')
             article_text = article.find('h3').text
             # step 1 - check if link is good
-            logger.debug(f'candidate reviews link: {article_href}')
+            # logger.debug(f'candidate reviews link: {article_href}')
             if ('market.yandex.' in article_href) and \
                     ('/reviews' in article_href) and \
-                    ('text?search' not in article_href):
-                logger.debug(f'passed the check for reviews: {article_href} -- {article_text}', )
+                    ('text?search' not in article_href) and \
+                    ('search?text' not in article_href):
+                # primary search branch - contains reviews
+                # logger.debug(f'passed PRIMARY the check for reviews: {article_href} -- {article_text}', )
                 id_2_href[article_id] = article_href
-                id_2_text[article_id] = article_text
+                id_2_text_primary[article_id] = article_text
                 # https://rapidfuzz.github.io/RapidFuzz/Usage/fuzz.html
-                id_2_text[article_id] = rapidfuzz.fuzz.token_set_ratio(
+                id_2_text_primary[article_id] = rapidfuzz.fuzz.token_set_ratio(
+                    user_query,
+                    article_href,  # seacrh against href because in google end text is truncated with dots
+                    processor=rapidfuzz.utils.default_process
+                )
+            elif ('market.yandex.' in article_href) and \
+                    ('/reviews' not in article_href) and \
+                    ('text?search' not in article_href) and \
+                    ('search?text' not in article_href):
+                # secondary seatch branch
+                # logger.debug(f'passed SECONDARY the check for reviews: {article_href} -- {article_text}', )
+                id_2_href[article_id] = article_href
+                id_2_text_secondary[article_id] = article_text
+                # https://rapidfuzz.github.io/RapidFuzz/Usage/fuzz.html
+                id_2_text_secondary[article_id] = rapidfuzz.fuzz.token_set_ratio(
                     user_query,
                     article_href,  # seacrh against href because in google end text is truncated with dots
                     processor=rapidfuzz.utils.default_process
                 )
         except:
             pass
-    if len(id_2_text) > 0:
-        logger.critical(sorted(id_2_text.items(), key=lambda x: x[1], reverse=True))
-        best_id, best_score = sorted(id_2_text.items(), key=lambda x: x[1], reverse=True)[0]
-        best_text = id_2_text[best_id]
+    if len(id_2_text_primary) > 0:
+        # logger.critical(sorted(id_2_text.items(), key=lambda x: x[1], reverse=True))
+        best_id, best_score = sorted(id_2_text_primary.items(), key=lambda x: x[1], reverse=True)[0]
+        best_text = id_2_text_primary[best_id]
+        best_href = id_2_href[best_id]
+    elif len(id_2_text_secondary) > 0:
+        best_id, best_score = sorted(id_2_text_secondary.items(), key=lambda x: x[1], reverse=True)[0]
+        best_text = id_2_text_secondary[best_id]
         best_href = id_2_href[best_id]
     else:
         # no results found
@@ -168,38 +193,88 @@ def search_google(user_query: str) -> str:
         page = browser.new_page()
         page.goto(url)
         content = page.content()
-        with open(f'{user_query}.html', 'w') as f:
-            f.write(content)
+        # with open(f'{user_query}.html', 'w') as f:
+        #     f.write(content)
         # with open("duck_res.html") as fp:
         #     soup = BeautifulSoup(fp, "html.parser")
         soup = BeautifulSoup(content, "html.parser")
-        logger.debug('in google_find_link')
+        # logger.debug('in google_find_link')
         best_link = google_find_link(soup=soup, user_query=user_query)
+        # logger.debug(f'best link: {best_link}')
         browser.close()
     return best_link
     # except Exception as e:
     #     logger.critical(f'error: {e}')
     #     return ''
 
+def thread_work(user_query):
+    '''
+    We don't know if the returned value actually links to reviews
+    '''
+    some_url = search_google(user_query=user_query)  # may return None if search fails
+    # logger.critical(f'some url: {some_url}')
+    if some_url:
+        if 'reviews' in some_url:
+            # logger.info('branch reviews')
+            # logger.info(user_query, )
+            reviews_url  = some_url
+            product_url = some_url[:some_url.find('/reviews')] + + '/reviews'
+            # logger.info(f'some_url: {some_url}')
+            # logger.info(f'reviews_url: {reviews_url}')
+            # logger.info(f'product_url: {product_url}')
+        elif '?sku=' in some_url:
+            # logger.warning('branch ?sku=')
+            # logger.warning(user_query, )
+            product_url = some_url[:some_url.find('?sku=')]
+            reviews_url = product_url + '/reviews'
+            # logger.warning(f'some_url: {some_url}')
+            # logger.warning(f'reviews_url: {reviews_url}')
+            # logger.warning(f'product_url: {product_url}')
+        else:
+            logger.critical(f'v1 - no results for: {user_query}')
+            return (user_query, [], [])
+        product_details_list = scrape_page_playwright(url_path=product_url, parse_func=parse_product)
+        review_details_list = scrape_page_playwright(url_path=reviews_url, parse_func=parse_reviews)
+        if not review_details_list[-1]:  # list of reviews
+            # this may happen if the reviews_url logic is faulty or when product names are inconsistent and redirects happen
+            prod_det = product_details_list[-1]
+            if prod_det:
+                correct_product_url = prod_det.get('product_url')
+                # logger.warning(correct_product_url)
+                if correct_product_url:
+                    correct_reviews_url = correct_product_url + '/reviews'
+                    review_details_list = scrape_page_playwright(url_path=correct_reviews_url, parse_func=parse_reviews)
+                    # logger.warning(correct_reviews_url)
+                    # logger.warning(review_details_list)
+        return (user_query, product_details_list, review_details_list)
+    logger.critical(f'v2 - no results for: {user_query}')
+    return (user_query, [], [])
+
 
 if __name__ == '__main__':
     sql_from_table = 'scraped_data.product_item_list'
-    where_clause = 'crawl_id <= 2 order by product_name asc limit 5'
+    where_clause = 'crawl_id <= 2 order by product_name'
     df = select_from_db.select_data(table=sql_from_table, where=where_clause)
     product_names = df['product_name'].values.tolist()
 
-    # user_query = 'Xiaomi 14'
-    pairs = []
-    for i, user_query in enumerate(product_names):
-        logger.warning(user_query)
-        reviews_url = search_google(user_query=user_query)  # may return None if DGG search fails
-        if reviews_url:
-            product_url = reviews_url.strip('/reviews')
-            review_details_list = asyncio.run(scrape_page_playwright(url_path=reviews_url, parse_func=parse_reviews))
-            product_details_list = asyncio.run(scrape_page_playwright(url_path=product_url, parse_func=parse_product))
-            pairs.append((product_details_list, review_details_list))
-            logger.info(product_details_list)
-    with open('../out/pairs.pkl', 'wb') as f:
+    ex = pool.ThreadPoolExecutor(max_workers=16,
+                                 thread_name_prefix='thread_',
+                                 initializer=None, initargs=())
+
+    with ex as executor:
+        future_to_url = {executor.submit(thread_work, user_query): user_query for user_query in product_names}
+
+    pairs = {}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+            pairs[url] = data
+        except Exception as exc:
+            print(f'{url} сгенерировано исключение: {exc}')
+
+    logger.info(type(pairs))
+    logger.info(type(data))
+    logger.info(len(pairs))
+    with open('../out/future_pairs.pkl', 'wb') as f:
         pickle.dump(pairs, f)
-    # logger.warning(pairs)
-    logger.warning(len(pairs))
