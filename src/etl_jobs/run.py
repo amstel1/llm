56aec41789a00d95023af594343d00132e5c256d
amstel <spareemail@yanex.ru>
2024-06-20 15:59:39 +0300

    abstracted step1 n etl

--- src/etl_jobs/run.py
+++ src/etl_jobs/run.py
@@ -11,8 +11,8 @@ import yaml
 from loguru import logger
 
 ## config part
-SCHEMA_NAME = 'mobile'
-PRODUCT_TYPE_NAME = 'Мобильный телефон'  # Стиральная машина, Холодильник
+SCHEMA_NAME = 'kettle'
+PRODUCT_TYPE_NAME = 'Чайник'  # Стиральная машина, Холодильник
 
 
 # step 1
@@ -252,16 +252,202 @@ class ReviewsProductDetailsDo(Do):
         return {"step_0": df}
 
 
+class Step1:
+    def __init__(self):
+        kettle = {
+            'product_type_name': 'Чайник',
+            'schema_name': 'kettle',
+            'shop_slug': 'chayniki',
+            'shop_max_page': 106,
+            'vek21_slug': 'teapots',
+            'vek21_max_page': 31,
+            'onliner_slug': 'kettle',
+            'onliner_max_page': 120,
+        }
+
+        vacuumcleaner = {
+            'product_type_name': 'Пылесос',
+            'schema_name': 'vacuumcleaner',
+            'shop_slug': 'pylesosy',
+            'shop_max_page': 106,
+            'vek21_slug': 'vacuum',
+            'vek21_max_page': 24,
+            'onliner_slug': 'vacuumcleaner',
+            'onliner_max_page': 136,
+        }
+
+        headphones = {
+            'product_type_name': 'Наушники',
+            'schema_name': 'headphones',
+            'shop_slug': 'naushniki',
+            'shop_max_page': 106,
+            'vek21_slug': 'headphones',
+            'vek21_max_page': 42,
+            'onliner_slug': 'headphones',
+            'onliner_max_page': 226,
+        }
+
+        smartwatch = {
+            'product_type_name': 'Умные часы',
+            'schema_name': 'smartwatch',
+            'shop_slug': 'umnie_chasi',
+            'shop_max_page': 106,
+            'vek21_slug': 'smart_watches',
+            'vek21_max_page': 9,
+            'onliner_slug': 'smartwatch',
+            'onliner_max_page': 32,
+        }
+
+        dishwasher = {
+            'product_type_name': 'Посудомойка',
+            'schema_name': 'dishwasher',
+            'shop_slug': 'posudomoechnye_mashiny',
+            'shop_max_page': 61,
+            'vek21_slug': 'dishwashers',
+            'vek21_max_page': 10,
+            'onliner_slug': 'dishwasher',
+            'onliner_max_page': 77,
+        }
+
+        hob_cooker = {
+            'product_type_name': 'Варочная панель',
+            'schema_name': 'hob_cooker',
+            'shop_slug': 'varochnie_paneli',
+            'shop_max_page': 106,
+            'vek21_slug': 'hobs',
+            'vek21_max_page': 25,
+            'onliner_slug': 'hob_cooker',
+            'onliner_max_page': 167,
+        }
+
+        oven_cooker = {
+            'product_type_name': 'Духовой шкаф',
+            'schema_name': 'oven_cooker',
+            'shop_slug': 'duhovye_shkafy',
+            'shop_max_page': 106,
+            'vek21_slug': 'ovens',
+            'vek21_max_page': 18,
+            'onliner_slug': 'oven_cooker',
+            'onliner_max_page': 127,
+        }
+
+        iron = {
+            'product_type_name': 'Утюг',
+            'schema_name': 'iron',
+            'shop_slug': 'utyugi',
+            'shop_max_page': 56,
+            'vek21_slug': 'irons',
+            'vek21_max_page': 13,
+            'onliner_slug': 'iron',
+            'onliner_max_page': 57,
+        }
+
+        conditioner = {
+            'product_type_name': 'Кондиционер',
+            'schema_name': 'conditioner',
+            'shop_slug': 'konditsionery',
+            'shop_max_page': 102,
+            'vek21_slug': 'conditioners',
+            'vek21_max_page': 25,
+            'onliner_slug': 'conditioners',
+            'onliner_max_page': 37,
+        }
+
+        waterheater = {
+            'product_type_name': 'Водонагреватель',
+            'schema_name': 'waterheater',
+            'shop_slug': 'vodonagrevateli',
+            'shop_max_page': 84,
+            'vek21_slug': 'waterheaters',
+            'vek21_max_page': 27,
+            'onliner_slug': 'waterheater',
+            'onliner_max_page': 46,
+        }
+
+        microvawe = {
+            'product_type_name': 'Микроволновка',
+            'schema_name': 'microvawe',
+            'shop_slug': 'mikrovolnovye_pechi',
+            'shop_max_page': 68,
+            'vek21_slug': 'microwaves',
+            'vek21_max_page': 12,
+            'onliner_slug': 'microvawe',
+            'onliner_max_page': 73,
+        }
+
+        self.products = [
+            vacuumcleaner,
+            headphones,
+            smartwatch,
+            dishwasher,
+            hob_cooker,
+            oven_cooker,
+            iron,
+            conditioner,
+            waterheater,
+            microvawe,
+        ]
+
+    def run(self):
+        # step 1. ItemList from sites to Postgres. Not: all three for each new product
+        for product in self.products:
+            product_type_url = [f'https://shop.by/{product["shop_slug"]}/?page_id={i}' for i in range(1, {product["shop_max_page"]})]  # 1,30
+            this_job = Job(
+                reader=EcomItemListRead(extractor_name='ShopByExtractor',
+                                        product_type_url=product_type_url,
+                                        product_type_name={product["product_type_name"]}),
+                processor=ItemListDo(),
+                writer=PostgresDataFrameWrite(
+                    schema_name={product["schema_name"]},
+                    table_name='product_item_list',  # product_item_list_to_fill, product_item_list
+                    insert_unique=True,
+                    index_column="product_url",
+                    if_exists='append')
+            )
+            this_job.run()
+
+            product_type_url=[f'https://www.21vek.by/{product["vek21_slug"]}/page:{i}/' for i in range(1, {product["vek21_max_page"]})]  # 2,11
+            this_job = Job(
+                reader=EcomItemListRead(extractor_name='Vek21Extractor',
+                                        product_type_url=product_type_url,
+                                        product_type_name={product["product_type_name"]}),
+                processor=ItemListDo(),
+                writer=PostgresDataFrameWrite(
+                    schema_name={product["schema_name"]},
+                    table_name='product_item_list',  # product_item_list_to_fill, product_item_list
+                    insert_unique=True,
+                    index_column="product_url",
+                    if_exists='append'
+                ),
+            )
+            this_job.run()
+
+            product_type_url=[f'https://catalog.onliner.by/{product["onliner_slug"]}?page={i}' for i in range(1, {product["onliner_max_page"]})]  # 2,50
+            this_job = Job(
+                reader=EcomItemListRead(extractor_name='OnlinerExtractor',
+                                        product_type_url=product_type_url,
+                                        product_type_name={product["product_type_name"]}),
+                processor=ItemListDo(),
+                writer=PostgresDataFrameWrite(
+                    schema_name={product["schema_name"]},
+                    table_name='product_item_list',  # product_item_list_to_fill, product_item_list
+                    insert_unique=True,
+                    index_column="product_url",
+                    if_exists='append')
+            )
+            this_job.run()
+
+
+
 if __name__ == '__main__':
     pass
 
     # step 1. ItemList from sites to Postgres. Not: all three for each new product
 
+    # product_type_url = [f'https://shop.by/chayniki/?page_id={i}' for i in range(1, 106)]  # 1,30
+    # product_type_url=[f'https://www.21vek.by/teapots/page:{i}/' for i in range(2, 31)]  # 2,11
+    # product_type_url=[f'https://catalog.onliner.by/kettle?page={i}' for i in range(86, 120)]  # 2,50
 
-    # # product_type_url = [f'https://shop.by/chayniki/?page_id={i}' for i in range(1, 106)]  # 1,30
-    # # product_type_url=[f'https://www.21vek.by/teapot/page:{i}/' for i in range(1, 8+1)]  # 2,11
-    # # product_type_url=[f'https://catalog.onliner.by/kettle?page={i}' for i in range(1, 85+1)]  # 2,50
-    #
     # ItemlList_2_Postgres = Job(
     #     # reader=EcomItemListRead(extractor_name='ShopByExtractor', product_type_url=product_type_url, product_type_name=PRODUCT_TYPE_NAME),
     #     # reader=EcomItemListRead(extractor_name='Vek21Extractor', product_type_url=product_type_url, product_type_name=PRODUCT_TYPE_NAME),
@@ -296,7 +482,7 @@ if __name__ == '__main__':
     # ItemDetails_2_Postgres.run()
     # logger.warning('End - Job 2')
 
-    # step 2.5 - записать в очередь
+    # # step 2.5 - записать в очередь
     mongo_read_product_reviews = MongoRead(operation='read', db_name=SCHEMA_NAME, collection_name='product_reviews')
     mongo_read_product_details = MongoRead(operation='read', db_name=SCHEMA_NAME, collection_name='product_details')
     postgres_read_item_list = PostgresDataFrameRead(
--- src/etl_jobs/utils.py
+++ src/etl_jobs/utils.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from postgres.utils import PostgresDataFrameRead
 from web_scraping.utils import EcomProductRead
 from loguru import logger
-
+import attribute_mappings
 
 class ItemListDo(Do):
     '''
@@ -28,13 +28,26 @@ class ItemListDo(Do):
 class ItemDetailsDo(Do):
     def __init__(self, product_type_name='Холодильник'):
         self.product_type_name = product_type_name
-        self.handler_mappring = {
-            'Стиральная машина': self.washing_mashine_handler,
-            'Холодильник': self.fridge_handler,
-            'Телевизор': self.tv_handler,
-            'Мобильный телефон': self.mobile_handler,
+        self.handler_mapping = {
+            'Стиральная машина': attribute_mappings.washing_machine_mapping,
+            'Холодильник': attribute_mappings.fridge_mapping,
+            'Телевизор': attribute_mappings.tv_mapping,
+            'Мобильный телефон': attribute_mappings.mobile_mapping,
+            'Чайник': attribute_mappings.kettle_mapping,
+
+            'Пылесос': attribute_mappings.vacuumcleaner_mapping,
+            'Наушники': attribute_mappings.headphones_mapping,
+            'Умные часы': attribute_mappings.smartwatch_mapping,
+            'Посудомойка': attribute_mappings.dishwasher_mapping,
+            'Варочная панель': attribute_mappings.hob_cooker_mapping,
+            'Духовой шкаф': attribute_mappings.oven_cooker_mapping,
+            'Утюг': attribute_mappings.iron_mapping,
+            'Кондиционер': attribute_mappings.conditioner_mapping,
+            'Водонагреватель': attribute_mappings.waterheater_mapping,
+            'Микроволновка': attribute_mappings.microwave_mapping,
+
         }
-        assert self.product_type_name in self.handler_mappring
+        assert self.product_type_name in self.handler_mapping
 
     @staticmethod
     def remove_from_bracket(s):
@@ -44,279 +57,19 @@ class ItemDetailsDo(Do):
                 return s[:position].strip(' ')
         return s
 
-    def mobile_handler(self, df: pd.DataFrame):
-        shop_mapping = {
-            'name': 'name',
-            'product_url': 'product_url',
-            'offer_count': 'offer_count',
-            'min_price': 'min_price',
-            "Производитель": "manufacturer",
-            "Дата выхода": "release_date",
-            "Тип устройства": "device_type",
-            "Вид устройства": "device_category",
-            "Платформа": "platform",
-            "Версия ОС": "os_version",
-            "Серия": "series",
-            "Стандарт связи": "network_standard",
-            "Количество SIM-карт": "num_sim_cards",
-            "Тип SIM-карты": "sim_type",
-            "Диагональ экрана, \"": "screen_diagonal_inch",
-            "Разрешение экрана, точек": "screen_resolution_px",
-            "Технология экрана": "screen_technology",
-            "Частота обновления экрана": "refresh_rate_hz",
-            "Оперативная память": "ram_gb",
-            "Встроенная память": "internal_storage_gb",
-            "Поддержка карт памяти": "memory_card_support",
-            "Камера": "camera",
-            "Количество основных камер": "num_main_cameras",
-            "Разрешение основной камеры, Мп": "main_camera_resolution_mp",
-            "Максимальное разрешение видео": "max_video_resolution",
-            "Количество кадров в секунду": "fps",
-            "Беспроводная зарядка": "wireless_charging",
-            "Быстрая зарядка": "fast_charging",
-            "Обратная беспроводная зарядка": "reverse_wireless_charging",
-            "Модули камеры": "camera_modules",
-            "Конструкция корпуса": "body_construction",
-            "Материал корпуса": "body_material",
-            "Цвет корпуса": "body_color",
-            "Ударопрочный корпус": "shockproof_body",
-            "Пыле- и влагозащита": "dust_water_resistance",
-            "Степень защиты (IP)": "ip_rating",
-            "Высота, см": "height_cm",
-            "Ширина, см": "width_cm",
-            "Толщина, см": "thickness_cm",
-            "Вес, г": "weight_g",
-            "Число пикселей на дюйм, ppi": "ppi",
-            "Соотношение сторон": "aspect_ratio",
-            "Сенсорный экран": "touch_screen",
-            "Защита от царапин": "scratch_protection",
-            "Постоянная работа экрана": "always_on_display",
-            "Производитель процессора": "processor_manufacturer",
-            "Процессор": "processor",
-            "Количество ядер процессора": "num_processor_cores",
-            "Техпроцесс": "process_technology",
-            "Диафрагма": "aperture",
-            "Вспышка": "flash",
-            "Автофокус": "autofocus",
-            "Оптическая стабилизация": "optical_stabilization",
-            "Оптический зум": "optical_zoom",
-            "Макс. количество кадров в секунду": "max_fps",
-            "Количество фронтальных камер": "num_front_cameras",
-            "Разрешение фронтальной камеры, Мп": "front_camera_resolution_mp",
-            "Диафрагма фронтальной камеры": "front_camera_aperture",
-            "Вспышка фронтальной камеры": "front_camera_flash",
-            "Автофокус фронтальной камеры": "front_camera_autofocus",
-            "Разрешение видео фронтальной камеры": "front_camera_video_resolution",
-            "Стереодинамики": "stereo_speakers",
-            "Безопасность": "security_features",
-            "Навигация": "navigation",
-            "Беспроводные интерфейсы": "wireless_interfaces",
-            "Разъемы": "ports",
-            "Емкость аккумулятора, мАч": "battery_capacity_mah",
-            "Аккумулятор": "battery",
-            "Время воспроизведения видео, ч": "video_playback_time_h",
-            "Время воспроизведения аудио, ч": "audio_playback_time_h",
-            "Комплект поставки": "package_contents"
-        }
-
-        shop_tv_inverse_mapping = {v: k for k, v in shop_mapping.items()}
-        cols = shop_mapping.keys()
-
+    def handler(self, df: pd.DataFrame):
+        shop_mapping = self.handler_mapping[self.product_type_name]
+        cols = [x for x in shop_mapping.keys() if x in df.columns]
         df = df[cols]
         df.rename(columns=shop_mapping, inplace=True)
-
         for col in df.columns:
             df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
             try:
                 df[col] = df[col].astype(float)
             except Exception as e:
                 df[col] = df[col].replace({'Есть': 'Да'})
-
-        # for col in ['offer_count', 'min_price',
-        #             ]:
-        #     df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-        #     df[col] = df[col].astype(float)
-        # for col in []:
-        #     df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-        #     df[col] = df[col].replace({'Есть': 'Да'})
         return df
 
-    def tv_handler(self, df: pd.DataFrame):
-        shop_tv_mapping = {
-            'name': 'name',
-            'product_url': 'product_url',
-            'offer_count': 'offer_count',
-            'min_price': 'min_price',
-            "Производитель": "manufacturer",
-            "Дата выхода": "release_date",
-            "Тип": "type",
-            "Диагональ экрана, \"": "screen_diagonal_inch",
-            "Разрешение экрана": "screen_resolution",
-            "Частота матрицы, Гц": "refresh_rate_hz",
-            "Изогнутый экран": "curved_screen",
-            "Поддержка 3D": "support_3d",
-            "Smart TV": "smart_tv",
-            "Платформа Smart TV": "smart_tv_platform",
-            "Версия системы": "system_version",
-            "Безрамочный дизайн": "frameless_design",
-            "Цвет корпуса": "body_color",
-            "Цвет рамки": "frame_color",
-            "Цвет подставки": "stand_color",
-            "Подставка": "stand",
-            "Крепление на стену": "wall_mount",
-            "Глубина цвета": "color_depth",
-            "Расширенный динамический диапазон (HDR)": "hdr",
-            "Форматы HDR": "hdr_formats",
-            "Игровые функции": "gaming_features",
-            "Процессор": "processor",
-            "Фоновая подсветка": "backlight",
-            "Голосовое управление": "voice_control",
-            "ТВ-тюнер": "tv_tuner",
-            "Сабвуфер": "subwoofer",
-            "Звуковая панель (саундбар)": "soundbar",
-            "Мощность звуковой системы, Вт": "sound_system_power_w",
-            "Количество динамиков": "num_speakers",
-            "Поддержка аудиокодеков объемного звука": "surround_sound_codec_support",
-            "Поддержка HDMI eARC": "hdmi_earc_support",
-            "Беспроводные интерфейсы": "wireless_interfaces",
-            "Версия Bluetooth": "bluetooth_version",
-            "Стандарт Wi-Fi": "wifi_standard",
-            "Разъемы": "ports",
-            "Кол-во разъемов HDMI": "num_hdmi_ports",
-            "Версия HDMI": "hdmi_version",
-            "Кол-во разъемов USB": "num_usb_ports",
-            "Smart-пульт": "smart_remote",
-            "Настенное крепление": "wall_mounting",
-            "Крепление VESA": "vesa_mount",
-            "Ширина, см": "width_cm",
-            "Высота (с подставкой), см": "height_with_stand_cm",
-            "Глубина (с подставкой), см": "depth_with_stand_cm",
-            "Высота (без подставки), см": "height_without_stand_cm",
-            "Толщина панели, см": "panel_thickness_cm",
-            "Вес (с подставкой), кг": "weight_with_stand_kg",
-            "Вес (без подставки), кг": "weight_without_stand_kg"
-        }
-
-        shop_tv_inverse_mapping = {v: k for k, v in shop_tv_mapping.items()}
-        cols = shop_tv_mapping.keys()
-
-        df = df[cols]
-        df.rename(columns=shop_tv_mapping, inplace=True)
-
-        for col in ['offer_count','min_price', 'release_date', 'screen_diagonal_inch', 'refresh_rate_hz', 'sound_system_power_w',
-                    'num_speakers', 'bluetooth_version', 'num_hdmi_ports', 'num_usb_ports', 'width_cm', 'height_with_stand_cm',
-                    'depth_with_stand_cm', 'height_without_stand_cm', 'panel_thickness_cm', 'weight_with_stand_kg', 'weight_without_stand_kg'
-                    ]:
-            df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-            df[col] = df[col].astype(float)
-        for col in ['curved_screen', 'support_3d', 'smart_tv', 'frameless_design', 'wall_mount', 'hdr', 'gaming_features',
-                    'backlight', 'voice_control', 'subwoofer', 'soundbar', 'hdmi_earc_support', 'smart_remote', 'wall_mounting']:
-            df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-            df[col] = df[col].replace({'Есть': 'Да'})
-        return df
-
-    def fridge_handler(self, df: pd.DataFrame):
-        shop_fridge_mapping = {
-            'name': 'name',
-            'product_url': 'product_url',
-            'offer_count': 'offer_count',
-            'min_price': 'min_price',
-            "Производитель": "manufacturer",
-            "Количество камер": "number_of_chambers",
-            "Тип": "type",
-            "Компоновка": "layout",
-            "No Frost": "no_frost",
-            "Зона свежести (BioFresh)": "biofresh_zone",
-            "Цвет корпуса": "body_color",
-            "Климатический класс": "climate_class",
-            "Возможность перенавешивания дверей": "door_reversibility",
-            "Дополнительная комплектация холодильного отделения": "additional_fridge_features",
-            "Ручки": "handles",
-            "Класс энергопотребления": "energy_class",
-            "Инверторный компрессор": "inverter_compressor",
-            "Тип управления": "control_type",
-            "Расположение блока управления": "control_panel_location",
-            "Дисплей": "display",
-            # numeric
-            "Количество компрессоров": "number_of_compressors",
-            "Объем холодильной камеры, л": "fridge_volume_l",
-            "Объем морозильной камеры, л": "freezer_volume_l",
-            "Количество отделений морозильной камеры": "number_of_freezer_compartments",
-            "Мощность замораживания, кг/сутки": "freezing_capacity_kg_per_day",
-            "Высота, см": "height_cm",
-            "Ширина, см": "width_cm",
-            "Глубина, см": "depth_cm"
-        }
-
-        shop_fridge_inverse_mapping = {v: k for k, v in shop_fridge_mapping.items()}
-        cols = shop_fridge_mapping.keys()
-        # logger.debug(cols)
-
-        df = df[cols]
-        df.rename(columns=shop_fridge_mapping, inplace=True)
-
-        for col in ['offer_count','min_price', 'number_of_compressors', 'fridge_volume_l', 'freezer_volume_l',
-                    'number_of_freezer_compartments', 'freezing_capacity_kg_per_day', 'height_cm', 'width_cm',
-                    'depth_cm']:
-            df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-            df[col] = df[col].astype(float)
-        for col in ['display', 'inverter_compressor', 'door_reversibility', 'biofresh_zone', 'no_frost']:
-            df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-            df[col] = df[col].replace({'Есть': 'Да'})
-        return df
-
-    def washing_mashine_handler(self, df: pd.DataFrame):
-        # logger.debug(df.head())
-        shop_washing_machine_inverse_mapping = {
-            'name': 'name',
-            'product_url': 'product_url',
-            'offer_count': 'offer_count',
-            'min_price': 'min_price',
-            "brand": "Производитель",
-            "load_type": "Тип загрузки",
-            "max_load": "Максимальная загрузка белья, кг",
-            "depth": "Глубина, см",
-            "width": "Ширина, см",
-            "height": "Высота, см",
-            "weight": "Вес, кг",
-            "programs_number": "Количество программ",
-            "has_display": "Дисплей",
-            "installation_type": "Установка",
-            "color": "Цвет",
-            "washing_class": "Класс стирки",
-            "additional_rinsing": "Дополнительное полоскание",
-            "max_rpm": "Максимальное количество оборотов отжима, об/мин",
-            "spinning_class": "Класс отжима",
-            "spinning_speed_selection": "Выбор скорости отжима",
-            "drying": "Сушка",
-            "energy_class": "Класс энергопотребления",
-            "addition_features": "Дополнительные функции",
-            "can_add_clothes": "Возможность дозагрузки белья",
-            "cancel_spinning": "Отмена отжима",
-            "light_ironing": "Программа «легкая глажка»",
-            "direct_drive": "Прямой привод (direct drive)",
-            "inverter_motor": "Инверторный двигатель",
-            "safety_features": "Безопасность",
-            "water_consumption": "Расход воды за стирку, л",
-        }
-        shop_washing_machine_mapping = {v: k for k, v in shop_washing_machine_inverse_mapping.items()}
-        cols = shop_washing_machine_mapping.keys()
-        # logger.debug(cols)
-
-        df = df[cols]
-        df.rename(columns=shop_washing_machine_mapping, inplace=True)
-
-        for col in ['min_price', 'max_load', 'depth', 'width', 'height', 'weight', 'max_rpm', 'water_consumption',
-                    'programs_number']:
-            df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-            df[col] = df[col].astype(float)
-        for col in ['has_display', 'additional_rinsing',
-                    'spinning_speed_selection', 'drying',
-                    'can_add_clothes', 'cancel_spinning',
-                    'light_ironing', 'direct_drive', 'inverter_motor']:
-            df[col] = df[col].map(ItemDetailsDo.remove_from_bracket)
-            df[col] = df[col].replace({'Есть': 'Да'})
-        return df
 
     def process(self, data: Dict[StepNum, Any]) -> Dict[StepNum, Any]:
         # logger.critical(data)
@@ -324,7 +77,7 @@ class ItemDetailsDo(Do):
         data = data.get("step_0")
         if isinstance(data, list) and not (isinstance(data[0], list) or isinstance(data[0], tuple)):
             df = pd.DataFrame(data)
-            df = self.handler_mappring[self.product_type_name](df)
+            df = self.handler(df)
             return {"step_0": df}
         else:
             return {"step_0": None}
